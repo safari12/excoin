be95a8d7d74fd0a6ac3c3a0a4a80601e7035ace8
safari <rsafari.s@gmail.com>
2018-04-05 19:59:11 -0500

    added first test for block module

--- apps/blockchain/mix.exs
+++ apps/blockchain/mix.exs
@@ -23,7 +23,8 @@ defmodule Blockchain.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Blockchain.Application, []}
     ]
   end
 
---
+++ apps/blockchain/test/blockchain/block_test.exs
@@ -0,0 +1,15 @@
+defmodule Blockchain.BlockTest do
+  use ExUnit.Case, async: true
+
+  alias Blockchain.{Block, Chain}
+
+  test "generate next block" do
+    data = "some data"
+    b = Block.generate_next_block("some data")
+    latest_block = Chain.latest_block()
+
+    assert b.prev_hash == latest_block.hash
+    assert b.data == data
+    assert b.index == latest_block.index + 1
+  end
+end
