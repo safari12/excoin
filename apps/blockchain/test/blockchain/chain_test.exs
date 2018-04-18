1321276a71c757b388d0573a8bffb88e2324b845
safari <rsafari.s@gmail.com>
2018-04-17 22:07:19 -0500

    fixed test cases

--- apps/blockchain/test/blockchain/chain_test.exs
+++ apps/blockchain/test/blockchain/chain_test.exs
@@ -42,44 +42,44 @@ defmodule Blockchain.ChainTest do
     assert {:error, :invalid_block_hash} = Chain.add_block(invalid_block)
 
     invalid_block = %{valid_block | timestamp: 0}
-    assert {:error, :invalid_block_timestamp} = Chain.add_block(invalid_block) 
+    assert {:error, :invalid_block_timestamp} = Chain.add_block(invalid_block)
 
     invalid_hash = "F#{String.slice(valid_block.hash, 1..-1)}"
     invalid_block = %{valid_block | hash: invalid_hash}
     assert {:error, :proof_of_work_not_verified} = Chain.add_block(invalid_block)
   end
-  #
-  # test "validate a blockchain" do
-  #   invalid_genesis_block = %Block{
-  #     index: 1,
-  #     prev_hash: "0",
-  #     timestamp: 1_465_154_705,
-  #     data: "genesis block"
-  #   }
-  #
-  #   assert {:error, :no_genesis_block} = Chain.validate_chain([invalid_genesis_block])
-  #
-  #   genesis_block = Block.genesis_block()
-  #   chain = [genesis_block]
-  #
-  #   invalid_next_block = %Block{
-  #     index: 1,
-  #     prev_hash: "wrong",
-  #     timestamp: 1_465_154_706,
-  #     data: "first block"
-  #   }
-  #
-  #   assert {:error, :invalid_block_previous_hash} =
-  #           Chain.validate_chain([invalid_next_block | chain])
-  #
-  #   assert Chain.validate_chain(mock_blockchain(3))
-  # end
-  #
-  # test "replace the blockchain" do
-  #   new_chain = mock_blockchain(6)
-  #   :ok = Chain.replace_chain(new_chain)
-  #   assert Chain.all_blocks() == new_chain
-  # end
+
+  test "validate a blockchain" do
+    invalid_genesis_block = %Block{
+      index: 1,
+      prev_hash: "0",
+      timestamp: 1_465_154_705,
+      data: "genesis block"
+    }
+
+    assert {:error, :no_genesis_block} = Chain.validate_chain([invalid_genesis_block])
+
+    genesis_block = Block.genesis_block()
+    chain = [genesis_block]
+
+    invalid_next_block = %Block{
+      index: 1,
+      prev_hash: "wrong",
+      timestamp: 1_465_154_706,
+      data: "first block"
+    }
+
+    assert {:error, :invalid_block_previous_hash} =
+            Chain.validate_chain([invalid_next_block | chain])
+
+    assert Chain.validate_chain(mock_blockchain(3))
+  end
+  
+  test "replace the blockchain" do
+    new_chain = mock_blockchain(6)
+    :ok = Chain.replace_chain(new_chain)
+    assert Chain.all_blocks() == new_chain
+  end
 
   defp proof_of_work, do: Application.fetch_env!(:blockchain, :proof_of_work)
 
