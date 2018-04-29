9a6016b08bbadf034517f3d3f07e2156350af62d
safari <rsafari.s@gmail.com>
2018-04-29 14:24:11 -0500

    improved block module test cases

--- apps/blockchain/test/blockchain/block_test.exs
+++ apps/blockchain/test/blockchain/block_test.exs
@@ -3,16 +3,18 @@ defmodule Blockchain.BlockTest do
 
   alias Blockchain.{Block, Chain}
 
-  test "generate next block" do
-    data = "some data"
-    b = Block.generate_next_block("some data")
-    latest_block = Chain.latest_block()
+  describe "generate_next_block" do
+    test "should generate the next block for chain" do
+      data = "some data"
+      b = Block.generate_next_block("some data")
+      latest_block = Chain.latest_block()
 
-    assert b.prev_hash == latest_block.hash
-    assert b.data == data
-    assert b.index == latest_block.index + 1
-    assert b.difficulty == nil
-    assert b.timestamp > latest_block.timestamp
-    assert byte_size(b.hash) == 64
+      assert b.prev_hash == latest_block.hash
+      assert b.data == data
+      assert b.index == latest_block.index + 1
+      assert b.difficulty == nil
+      assert b.timestamp > latest_block.timestamp
+      assert byte_size(b.hash) == 64
+    end
   end
 end
